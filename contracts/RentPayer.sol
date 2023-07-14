fb735e8a1b4128a47b68063752b740b3a6f59294
saucerswaplabs <houston@saucerswap.finance>
2023-07-14 09:18:04 -0400

    removed periphery stuff

--- contracts/RentPayer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity =0.6.12;
-
-contract RentPayer {
-
-    receive() external payable {}
-}
--- migrations/2_uni_migrations.js
+++
@@ -1,5 +0,0 @@
-const UniswapV2Factory = artifacts.require("UniswapV2Factory");
-
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(UniswapV2Factory, accounts[0]);
-}; 
